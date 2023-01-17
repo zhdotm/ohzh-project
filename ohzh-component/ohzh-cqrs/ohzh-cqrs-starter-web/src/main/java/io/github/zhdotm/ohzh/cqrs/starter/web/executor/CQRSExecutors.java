1b8e4105b55bd1e9548c29e205bccacd9a9f9b50
毛志浩 <85991202+zhdotm@users.noreply.github.com>
2023-01-17 21:15:11 +0800

    refactor: 重构

--- ohzh-component/ohzh-cqrs/ohzh-cqrs-starter-web/src/main/java/io/github/zhdotm/ohzh/cqrs/starter/web/executor/CQRSExecutors.java
+++ ohzh-component/ohzh-cqrs/ohzh-cqrs-starter-web/src/main/java/io/github/zhdotm/ohzh/cqrs/starter/web/executor/CQRSExecutors.java
@@ -1,12 +1,21 @@
 package io.github.zhdotm.ohzh.cqrs.starter.web.executor;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.extra.spring.SpringUtil;
 import io.github.zhdotm.ohzh.biz.dto.BizScenario;
-import io.github.zhdotm.ohzh.cqrs.starter.web.CQRSExecutorFactory;
+import io.github.zhdotm.ohzh.cqrs.core.exception.CQRSException;
+import io.github.zhdotm.ohzh.cqrs.starter.web.enums.CQRSExceptionEnum;
 import lombok.SneakyThrows;
-import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * CQRS执行器
@@ -14,7 +23,7 @@ import java.util.function.Function;
  * @author zhihao.mao
  */
 
-@Component
+@Slf4j
 public class CQRSExecutors {
 
     /**
@@ -25,8 +34,12 @@ public class CQRSExecutors {
      */
     @SneakyThrows
     public static void registerQueryExecutor(String name, IQueryExecutor queryExe) {
+        Assert.isTrue(queryExe.isExistQueryExecutorAnnotation(), (Supplier<Throwable>) () -> new CQRSException(CQRSExceptionEnum.QUERY_EXECUTOR_ANNOTATION_NOT_EXIST.getCode(), CQRSExceptionEnum.QUERY_EXECUTOR_ANNOTATION_NOT_EXIST.getMessage(queryExe.getClass().getSimpleName())));
+        if (StrUtil.isBlank(name)) {
+            name = queryExe.getCode();
+        }
 
-        CQRSExecutorFactory.registerQueryExecutor(name, queryExe);
+        SpringUtil.registerBean(name, queryExe);
     }
 
     /**
@@ -37,8 +50,93 @@ public class CQRSExecutors {
      */
     @SneakyThrows
     public static void registerCommandExecutor(String name, ICommandExecutor cmdExe) {
+        Assert.isTrue(cmdExe.isExistCommandExecutorAnnotation(), (Supplier<Throwable>) () -> new CQRSException(CQRSExceptionEnum.COMMAND_EXECUTOR_ANNOTATION_NOT_EXIST.getCode(), CQRSExceptionEnum.COMMAND_EXECUTOR_ANNOTATION_NOT_EXIST.getMessage(cmdExe.getClass().getSimpleName())));
+        if (StrUtil.isBlank(name)) {
+            name = cmdExe.getCode();
+        }
 
-        CQRSExecutorFactory.registerCommandExecutor(name, cmdExe);
+        SpringUtil.registerBean(name, cmdExe);
+    }
+
+    /**
+     * 获取命令执行器
+     *
+     * @param bizScenario 业务身份
+     * @param cmdExeClazz 命令执行器类型
+     * @param <T>         命令执行器类型
+     * @return 命令执行器
+     */
+    @SneakyThrows
+    public static <T extends ICommandExecutor> T getCommandExecutor(BizScenario bizScenario, Class<T> cmdExeClazz) {
+        Collection<T> cmdExes = SpringUtil
+                .getBeansOfType(cmdExeClazz)
+                .values();
+
+        List<T> matchedCmdExes = cmdExes
+                .stream()
+                .filter(cmdExe -> cmdExe.strictMatch(bizScenario))
+                .collect(Collectors.toList());
+        if (CollectionUtil.isNotEmpty(matchedCmdExes)) {
+            Assert.isTrue(matchedCmdExes.size() == 1, (Supplier<Throwable>) () -> new CQRSException(CQRSExceptionEnum.COMMAND_EXECUTOR_STRICT_MATCH_MANY.getCode(), CQRSExceptionEnum.COMMAND_EXECUTOR_STRICT_MATCH_MANY.getMessage(bizScenario)));
+            T matchedCmdExe = matchedCmdExes.get(0);
+
+            log.info("严格匹配到命令执行器[{}]: {}", bizScenario, matchedCmdExe.getCode());
+            return matchedCmdExe;
+        }
+
+        matchedCmdExes = cmdExes.stream()
+                .filter(cmdExe -> cmdExe.looseMatch(bizScenario))
+                .collect(Collectors.toList());
+        if (CollectionUtil.isNotEmpty(matchedCmdExes)) {
+            Assert.isTrue(matchedCmdExes.size() == 1, (Supplier<Throwable>) () -> new CQRSException(CQRSExceptionEnum.COMMAND_EXECUTOR_LOOSE_MATCH_MANY.getCode(), CQRSExceptionEnum.COMMAND_EXECUTOR_LOOSE_MATCH_MANY.getMessage(bizScenario)));
+            T matchedCmdExe = matchedCmdExes.get(0);
+
+            log.info("松散匹配到命令执行器[{}]: {}", bizScenario, matchedCmdExe.getCode());
+            return matchedCmdExe;
+        }
+
+        throw new CQRSException(CQRSExceptionEnum.COMMAND_EXECUTOR_NOT_MATCH.getCode(), CQRSExceptionEnum.COMMAND_EXECUTOR_NOT_MATCH.getMessage(bizScenario));
+    }
+
+    /**
+     * 获取查询执行器
+     *
+     * @param bizScenario   业务身份
+     * @param queryExeClazz 查询执行器类型
+     * @param <T>           查询执行器类型
+     * @return 查询执行器
+     */
+    @SneakyThrows
+    public static <T extends IQueryExecutor> T getQueryExecutor(BizScenario bizScenario,
+                                                                Class<T> queryExeClazz) {
+        Collection<T> queryExes = SpringUtil
+                .getBeansOfType(queryExeClazz)
+                .values();
+
+        List<T> matchedQueryExes = queryExes
+                .stream()
+                .filter(queryExe -> queryExe.strictMatch(bizScenario))
+                .collect(Collectors.toList());
+        if (CollectionUtil.isNotEmpty(matchedQueryExes)) {
+            Assert.isTrue(matchedQueryExes.size() == 1, (Supplier<Throwable>) () -> new CQRSException(CQRSExceptionEnum.QUERY_EXECUTOR_STRICT_MATCH_MANY.getCode(), CQRSExceptionEnum.QUERY_EXECUTOR_STRICT_MATCH_MANY.getMessage(bizScenario)));
+            T matchedQueryExe = matchedQueryExes.get(0);
+            log.info("严格匹配到查询执行器[{}]: {}", bizScenario, matchedQueryExe.getCode());
+
+            return matchedQueryExe;
+        }
+
+        matchedQueryExes = queryExes.stream()
+                .filter(queryExe -> queryExe.looseMatch(bizScenario))
+                .collect(Collectors.toList());
+        if (CollectionUtil.isNotEmpty(matchedQueryExes)) {
+            Assert.isTrue(matchedQueryExes.size() == 1, (Supplier<Throwable>) () -> new CQRSException(CQRSExceptionEnum.QUERY_EXECUTOR_LOOSE_MATCH_MANY.getCode(), CQRSExceptionEnum.QUERY_EXECUTOR_LOOSE_MATCH_MANY.getMessage(bizScenario)));
+            T matchedQueryExe = matchedQueryExes.get(0);
+            log.info("松散匹配到查询执行器[{}]: {}", bizScenario, matchedQueryExe.getCode());
+
+            return matchedQueryExe;
+        }
+
+        throw new CQRSException(CQRSExceptionEnum.QUERY_EXECUTOR_NOT_MATCH.getCode(), CQRSExceptionEnum.QUERY_EXECUTOR_NOT_MATCH.getMessage(bizScenario));
     }
 
     /**
@@ -53,7 +151,7 @@ public class CQRSExecutors {
     public <T extends ICommandExecutor> void execute(BizScenario bizScenario,
                                                      Class<T> cmdExeClazz,
                                                      Consumer<T> cmdExeConsumer) {
-        T commandExecutor = CQRSExecutorFactory.getCommandExecutor(bizScenario, cmdExeClazz);
+        T commandExecutor = getCommandExecutor(bizScenario, cmdExeClazz);
 
         cmdExeConsumer.accept(commandExecutor);
     }
@@ -72,7 +170,7 @@ public class CQRSExecutors {
     public <T extends ICommandExecutor, R> R execute(BizScenario bizScenario,
                                                      Class<T> cmdExeClazz,
                                                      Function<T, R> cmdExeFunction) {
-        T commandExecutor = CQRSExecutorFactory.getCommandExecutor(bizScenario, cmdExeClazz);
+        T commandExecutor = getCommandExecutor(bizScenario, cmdExeClazz);
 
         return cmdExeFunction.apply(commandExecutor);
     }
@@ -90,7 +188,7 @@ public class CQRSExecutors {
     public <T extends IQueryExecutor, R> R query(BizScenario bizScenario,
                                                  Class<T> queryExeClazz,
                                                  Function<T, R> cmdExeConsumer) {
-        T commandExecutor = CQRSExecutorFactory.getQueryExecutor(bizScenario, queryExeClazz);
+        T commandExecutor = getQueryExecutor(bizScenario, queryExeClazz);
 
         return cmdExeConsumer.apply(commandExecutor);
     }
