55344dda9164b1927ffd51956bfa7e39d3ac5fdf
ANTHEM37 <85991202+zhdotm@users.noreply.github.com>
2023-11-29 01:13:24 +0800

    feat(流程中心): init

---
+++ ohzh-component/ohzh-flow/ohzh-flow-server/ohzh-flow-server-start/src/main/java/io/github/zhdotm/ohzh/flow/server/domain/start/FlowServerApplication.java
@@ -0,0 +1,17 @@
+package io.github.zhdotm.ohzh.flow.server.domain.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhihao.mao
+ */
+
+@SpringBootApplication
+public class FlowServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowServerApplication.class, args);
+    }
+    
+}
