11e191068546b2cc658709822c577ca6a9fe9eba
ANTHEM37 <85991202+zhdotm@users.noreply.github.com>
2023-11-30 01:32:11 +0800

    feat(流程中心): init

---
+++ ohzh-component/ohzh-flow/ohzh-flow-server/ohzh-flow-server-domain/src/main/java/io/github/zhdotm/ohzh/flow/server/domain/model/ProcessDefinition.java
@@ -0,0 +1,10 @@
+package io.github.zhdotm.ohzh.flow.server.domain.model;
+
+/**
+ * 流程定义
+ *
+ * @author zhihao.mao
+ */
+
+public class ProcessDefinition {
+}
---
+++ ohzh-component/ohzh-flow/ohzh-flow-server/ohzh-flow-server-domain/src/main/java/io/github/zhdotm/ohzh/flow/server/domain/model/ProcessTask.java
@@ -0,0 +1,10 @@
+package io.github.zhdotm.ohzh.flow.server.domain.model;
+
+/**
+ * 流程任务
+ *
+ * @author zhihao.mao
+ */
+
+public class ProcessTask {
+}
