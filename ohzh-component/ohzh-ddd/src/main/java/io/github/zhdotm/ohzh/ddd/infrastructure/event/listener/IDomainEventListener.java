5d1374d42e9c5108b4a4bbd2370bba89fa6a5959
ANTHEM37 <85991202+zhdotm@users.noreply.github.com>
2023-12-03 04:24:13 +0800

    feat(领域驱动组件): 重构领域驱动组件

--- ohzh-component/ohzh-ddd/src/main/java/io/github/zhdotm/ohzh/ddd/infrastructure/event/listener/IDomainEventListener.java
+++ ohzh-component/ohzh-ddd/src/main/java/io/github/zhdotm/ohzh/ddd/infrastructure/event/listener/IDomainEventListener.java
@@ -1,18 +1,31 @@
 package io.github.zhdotm.ohzh.ddd.infrastructure.event.listener;
 
 import cn.hutool.extra.spring.SpringUtil;
+import io.github.zhdotm.ohzh.ddd.application.service.IApplicationService;
 import io.github.zhdotm.ohzh.ddd.domain.aggregation.service.IDomainService;
 import io.github.zhdotm.ohzh.ddd.infrastructure.event.AbstractDomainEvent;
 import org.springframework.context.ApplicationListener;
 
 /**
- * 领域事件监听器
+ * 领域事件监听器（具体实现一般放在应用服务层）
  *
  * @author zhihao.mao
  */
 
 @FunctionalInterface
 public interface IDomainEventListener<E extends AbstractDomainEvent<?>> extends ApplicationListener<E> {
+
+    /**
+     * 获取领域服务
+     *
+     * @param clazz 类
+     * @param <T>   领域服务类型
+     * @return 领域服务
+     */
+    default <T extends IApplicationService> T getApplicationService(Class<T> clazz) {
+
+        return SpringUtil.getBean(clazz);
+    }
 
     /**
      * 获取领域服务
